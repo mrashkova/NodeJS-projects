cb3521e1d20f822013f03a6145240b8338c233fa
mrashkova <mrashkkova@gmail.com>
2023-10-10 16:10:53 +0300

    FW add new project, fix routing

---
+++ FriendlyWorld/src/controllers/homeController.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/search", (req, res) => {
+  res.render("search");
+});
+
+router.get("/404", (req, res) => {
+  res.render("404");
+});
+
+module.exports = router;
---
+++ FriendlyWorld/src/router.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const homeController = require("./controllers/homeController");
+
+router.use(homeController);
+
+router.get("*", (req, res) => {
+  res.redirect("/404");
+});
+
+module.exports = router;
